e672fad4cba162a8ca4cf5fade84ca796235358d
KazChecker <kazimierz.chlopek@hotmail.com>
2024-12-19 22:33:02 +0100

    Change webdriver to chromedriver

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -1,21 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-# Wskaż lokalizację binarek i ustaw tryb headless
+# Ustawienia opcji Chrome
 options = Options()
-options.binary_location = "/usr/bin/firefox"  # Lokalizacja Firefoksa w Ubuntu
-options.add_argument("--headless")  # Tryb headless (bez interfejsu graficznego)
+options.add_argument("--headless")  # Tryb headless
+options.add_argument("--no-sandbox")  # Wymagane w środowiskach CI
+options.add_argument("--disable-dev-shm-usage")  # Unikanie problemów z pamięcią
 
-# Lokalizacja Geckodrivera
-service = Service("/usr/local/bin/geckodriver", log_path="geckodriver.log")
-
-# Zwiększenie limitu czasu na inicjalizację
-WebDriver.W3C_REMOTE_PROTOCOL_TIMEOUT = 300  # Zwiększ limit z 120 do 300 sekund
+# Lokalizacja ChromeDrivera
+service = Service("/usr/bin/chromedriver")
 
 # Inicjalizacja WebDrivera
-driver = webdriver.Firefox(service=service, options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 # Przykładowy test otwierający stronę Google
 driver.get("https://www.google.com")
