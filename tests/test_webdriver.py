64ece175c50a1d858e8e650e60a2dc68a056739e
KazChecker <kazimierz.chlopek@hotmail.com>
2024-12-19 22:11:12 +0100

    Add GitHub Actions workflow for tests

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
-# Inicjalizacja WebDrivera
-driver = webdriver.Firefox()
+# Wskaż binarkę Firefoksa
+options = Options()
+options.binary_location = "/usr/bin/firefox"  # Domyślna lokalizacja w Ubuntu
+service = Service("/usr/local/bin/geckodriver")  # Lokalizacja geckodriver
 
-# Otwórz stronę Google
+# Inicjalizacja WebDrivera z pełną konfiguracją
+driver = webdriver.Firefox(service=service, options=options)
+
+# Testowanie (przykład otwarcia strony)
 driver.get("https://www.google.com")
 print("Page title is:", driver.title)
 
-# Zamknij przeglądarkę
 driver.quit()
