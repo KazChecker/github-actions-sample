c3a2d182a98fa77789ada0e817fa081bc7e8cb2f
KazChecker <kazimierz.chlopek@hotmail.com>
2024-12-19 22:26:24 +0100

    Enable headless mode for Selenium tests

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -1,18 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webdriver import WebDriver
 
-# Wskaż binarkę Firefoksa i dodaj tryb headless
+# Wskaż lokalizację binarek i ustaw tryb headless
 options = Options()
 options.binary_location = "/usr/bin/firefox"  # Lokalizacja Firefoksa w Ubuntu
-options.add_argument("--headless")  # Tryb headless
+options.add_argument("--headless")  # Tryb headless (bez interfejsu graficznego)
 
-service = Service("/usr/local/bin/geckodriver")  # Lokalizacja geckodriver
+# Lokalizacja Geckodrivera
+service = Service("/usr/local/bin/geckodriver", log_path="geckodriver.log")
+
+# Zwiększenie limitu czasu na inicjalizację
+WebDriver.W3C_REMOTE_PROTOCOL_TIMEOUT = 300  # Zwiększ limit z 120 do 300 sekund
 
 # Inicjalizacja WebDrivera
 driver = webdriver.Firefox(service=service, options=options)
 
-# Testowanie
+# Przykładowy test otwierający stronę Google
 driver.get("https://www.google.com")
 print("Page title is:", driver.title)
 
