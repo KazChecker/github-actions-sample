7b250683ac7630e71325fcfb530fb1ab86e09831
KazChecker <kazimierz.chlopek@hotmail.com>
2024-12-19 22:17:27 +0100

    Enable headless mode for Selenium tests

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -2,15 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
-# Wskaż binarkę Firefoksa
+# Wskaż binarkę Firefoksa i dodaj tryb headless
 options = Options()
-options.binary_location = "/usr/bin/firefox"  # Domyślna lokalizacja w Ubuntu
+options.binary_location = "/usr/bin/firefox"  # Lokalizacja Firefoksa w Ubuntu
+options.add_argument("--headless")  # Tryb headless
+
 service = Service("/usr/local/bin/geckodriver")  # Lokalizacja geckodriver
 
-# Inicjalizacja WebDrivera z pełną konfiguracją
+# Inicjalizacja WebDrivera
 driver = webdriver.Firefox(service=service, options=options)
 
-# Testowanie (przykład otwarcia strony)
+# Testowanie
 driver.get("https://www.google.com")
 print("Page title is:", driver.title)
 
