d3a84c2ee03271c3b2aa0d002277bca6569d03d7
KazChecker <kazimierz.chlopek@hotmail.com>
2024-12-19 22:41:03 +0100

    Change webdriver to chromedriver

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -2,20 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-# Ustawienia opcji Chrome
 options = Options()
-options.add_argument("--headless")  # Tryb headless
-options.add_argument("--no-sandbox")  # Wymagane w środowiskach CI
-options.add_argument("--disable-dev-shm-usage")  # Unikanie problemów z pamięcią
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-# Lokalizacja ChromeDrivera
 service = Service("/usr/bin/chromedriver")
-
-# Inicjalizacja WebDrivera
 driver = webdriver.Chrome(service=service, options=options)
 
-# Przykładowy test otwierający stronę Google
 driver.get("https://www.google.com")
-print("Page title is:", driver.title)
+assert "Google" in driver.title
 
 driver.quit()
