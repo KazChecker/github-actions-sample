85f6631ea9b714b0fab1205b6e4f6a82aab5dcef
KazChecker <kazimierz.chlopek@hotmail.com>
2024-12-19 22:49:02 +0100

    Change webdriver to chromedriver

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import shutil
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-service = Service("/usr/bin/chromedriver")
+chromedriver_path = shutil.which("chromedriver")
+service = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://www.google.com")
